25d10a2365bc38fc4c1d3a1291558b35bd16b3d8
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-11-12 23:10:04 +0700

    feat: add connect devtools

--- src/utils/graphql.ts
+++ src/utils/graphql.ts
@@ -6,4 +6,5 @@ import {
 export const apolloClient = new ApolloClient({
   uri: process.env.NEXT_PUBLIC_GRAPHQL_URL,
   cache: new InMemoryCache(),
+  connectToDevTools: JSON.parse(process.env.NEXT_PUBLIC_GRAPHQL_DEV_TOOLS || 'false'),
 })
