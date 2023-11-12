6a7c121e9f904b92e3c8cdd40cf599ac3390f1e9
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-11-12 16:15:04 +0700

    feat: update app page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { ApolloProvider } from '@apollo/client'
+import { apolloClient } from '@/utils/graphql'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>PBPA</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+
+      <ApolloProvider client={apolloClient}>
+        <Component {...pageProps} />
+      </ApolloProvider>
+    </>
+  )
 }
