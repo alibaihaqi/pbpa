408a156600c77de1a9f294ae5138112373d6aad6
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-11-12 16:15:14 +0700

    feat: update next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config, { webpack }) {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'globalThis.__DEV__': false,
+      })
+    );
+    return config
+  },
 }
 
 module.exports = nextConfig
