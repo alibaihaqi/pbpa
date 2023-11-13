64c9660297c479653aa07d00b54c62e59a2663d3
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-11-13 23:45:01 +0700

    feat: update interfaces

--- src/interfaces/contact/index.ts
+++ src/interfaces/contact/index.ts
@@ -1,13 +1,13 @@
 export interface IPhoneNumber {
-  __typename: string
+  __typename?: string
   number: string
 }
 
 export interface IContact {
-  id: number
+  id?: number
   first_name: string
   last_name: string
   created_at: string
   phones: IPhoneNumber[]
-  __typename: string
+  __typename?: string
 }
