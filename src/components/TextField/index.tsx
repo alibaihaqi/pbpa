60dd0762f5d73ca39de9e3be05b00e47cce09a82
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-11-13 23:44:23 +0700

    feat: add button component

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -1,37 +1,71 @@
 import { ChangeEvent, FC } from 'react'
 import styled from '@emotion/styled'
+import { IContactForm } from '@/constants/form'
+import { IPhoneNumber } from '@/interfaces/contact'
 
 interface ITextFieldProps {
   className?: string
-  input: string,
-  onChangeInput: (event: ChangeEvent<HTMLInputElement>) => void
-  title: string
+  form: IContactForm
+  onChangeInput?: (event: ChangeEvent<HTMLInputElement>) => void
+  onPhoneChangeInput?: (event: ChangeEvent<HTMLInputElement>, index: number, value: string) => void
+  phones?: IPhoneNumber[]
+  value?: string
 }
 
 const TextField: FC<ITextFieldProps> = ({
   className,
-  input,
+  form,
   onChangeInput,
-  title,
+  phones,
+  value,
 }) => {
-  return (
-    <div className={className}>
-      <p>{ title }</p>
+  const renderTextField = () => {
+    if (form.type === 'phone') {
+      phones?.map((phone, idx) => {
+        return (
+          <input
+            key={idx}
+            className="input fs-16"
+            type={form.keyboardType}
+            value={phone.number}
+            onChange={onChangeInput}
+          />
+        )
+      })
+    }
+
+    return (
       <input
         className="input fs-16"
-        value={input}
+        name={form.form_id}
+        type={form.keyboardType}
+        value={value as string}
         onChange={onChangeInput}
       />
+    )
+  }
+
+  return (
+    <div className={className}>
+      <p className='textfield-title'>
+        { form.title }
+      </p>
+
+      { renderTextField() }
     </div>
   )
 }
 
 export default styled(TextField)`
-  background: white;
+  background: transparent;
   display: flex;
-  padding: 8px; 
+  flex-direction: column;
   cursor: pointer;
   user-select: none;
+
+  .textfield-title {
+    padding-bottom: 8px;
+  }
   
   .input {
     border-radius: 4px;
