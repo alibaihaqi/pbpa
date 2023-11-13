b0bb941ce7bd14df194a954f034928d48632b75d
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-11-13 23:44:34 +0700

    feat: add constant

---
+++ src/constants/form.ts
@@ -0,0 +1,40 @@
+import { IPhoneNumber } from '@/interfaces/contact'
+
+export interface IContactForm {
+  form_id: string
+  id?: number
+  title: string
+  keyboardType: string
+  listPhone?: IPhoneNumber[]
+  type: string
+  value: string
+}
+
+export const CONTACT_FORMS: IContactForm[] = [
+  {
+    form_id: 'first_name',
+    title: 'First Name',
+    keyboardType: 'text',
+    type: 'text',
+    value: '',
+  },
+  {
+    form_id: 'last_name',
+    title: 'Last Name',
+    keyboardType: 'text',
+    type: 'text',
+    value: '',
+  },
+  {
+    form_id: 'phone',
+    title: 'Phone Number',
+    keyboardType: 'tel',
+    type: 'phone',
+    listPhone: [
+      {
+        number: '',
+      }
+    ],
+    value: '',
+  }
+]
