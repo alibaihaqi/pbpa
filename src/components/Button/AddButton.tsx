4d26831325bc6b88a7c77ff5e4af596f428405b4
Fadli Al Baihaqi <alibaihaqi1704@gmail.com>
2023-11-13 22:01:56 +0700

    feat: create add button

---
+++ src/components/Button/AddButton.tsx
@@ -0,0 +1,37 @@
+import { FC } from 'react'
+import styled from '@emotion/styled'
+
+interface IButtonProps {
+  className?: string
+  onClickHandler: () => void
+  title: string
+}
+
+const Button: FC<IButtonProps> = ({
+  className,
+  onClickHandler,
+  title,
+}) => {
+  return (
+    <button
+      className={className}
+      onClick={onClickHandler}
+    >
+      { title }
+    </button>
+  )
+}
+
+export default styled(Button)`
+  background-color: #E9E3E6;
+  background-repeat: no-repeat;
+  border: none;
+  border-radius: 8px;
+  color: #191308;
+  cursor: pointer;
+  font-size: 16px;
+  overflow: hidden;
+  outline: none;
+  padding: 8px 24px;
+
+`
